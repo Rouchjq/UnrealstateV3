8b5a94596ae3978317b5bf272ab0e1873c93589f
Jorge Quintero <rouchjq8@gmail.com>
2022-02-13 19:06:12 -0400

    Sea rreglo pero no se q xd

--- src/components/layout/Homesection4/index.jsx
+++ src/components/layout/Homesection4/index.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import style from './index.module.css';
-import img from '../../../assets/images/homesection4/img0.png'
-
+import React from "react";
+import style from "./index.module.css";
 
 export const Homesection4 = () => {
   return (
     <div className={style.back}>
-        
-        <div className={style.contenedor_principal}>
-            <div className={style.lineUp}></div>
-            <h1>Una plataforma que lo hace todo por ti</h1>
-            <p>Desde el momento en que se lista una propiedad hasta el cierre del trato<br />Unrealstate gestiona las transacciones de inmuebles todo en un sólo luga</p>
-            <button className={style.btn_grande}>Empezar</button>
-        </div>
-
+      <div className={style.contenedor_principal}>
+        <div className={style.lineUp}></div>
+        <h1>Una plataforma que lo hace todo por ti</h1>
+        <p>
+          Desde el momento en que se lista una propiedad hasta el cierre del
+          trato
+          <br />
+          Unrealstate gestiona las transacciones de inmuebles todo en un sólo
+          luga
+        </p>
+        <button className={style.btn_grande}>Empezar</button>
+      </div>
     </div>
-  )
-}
+  );
+};

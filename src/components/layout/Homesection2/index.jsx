89b4a4b8b6ea6af5056f3a882d4cc646d2e8703d
Jorge Quintero <rouchjq8@gmail.com>
2022-02-27 15:38:05 -0400

    Seccion 2
    Se ajusto el texto para mobile

--- src/components/layout/Homesection2/index.jsx
+++ src/components/layout/Homesection2/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import style from './index.module.css';
+import React from "react";
+import style from "./index.module.css";
 
 export const Homesection2 = () => {
   return (
@@ -8,12 +8,9 @@ export const Homesection2 = () => {
       <h1>El futuro del inmueble</h1>
       <p>
         Unrealstate lo hace todo por ti. Desde el momento que se lista una
-        propiedad hasta cerrar el acuerdo
-        <br />
-        con las firmas legales a través de contratos automatizados con
-        tecnología blockchain.
-        <br />
-        Vende, compra o invierte en propiedades
+        propiedad hasta cerrar el acuerdo con las firmas legales a través de
+        contratos automatizados con tecnología blockchain. Vende, compra o
+        invierte en propiedades
       </p>
     </section>
   );

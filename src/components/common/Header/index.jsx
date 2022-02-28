efa546d9e7fef823b6a446ce21933d3a2382eafe
Jorge Quintero <rouchjq8@gmail.com>
2022-02-28 16:31:26 -0400

    ajustes al header y la seccion 1
    Se ajusto el header y la seccion 1 para que no chocaran en el dominio

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -5,19 +5,21 @@ import Lmobile from "../../../assets/images/header/logo-mobile.png";
 export const Header = () => {
   return (
     <header className={style.menu_principal}>
-      <div id="logo" className={style.logo}>
-        <img src={logo} alt="logo" className={style.limg} />
-        <img src={Lmobile} alt="logo mobile" className={style.lmobile} />
-      </div>
+      <div className={style.container}>
+        <div id="logo" className={style.logo}>
+          <img src={logo} alt="logo" className={style.limg} />
+          <img src={Lmobile} alt="logo mobile" className={style.lmobile} />
+        </div>
 
-      <div className={style.botones}>
-        <button className={style.btn_grande}>Empezar</button>
-        <button className={style.btn_lng}>Ing</button>
-        <button className={style.btn_lng}>Esp</button>
-        <a href="#" className={style.btn_mobile}>
-          {" "}
-          <i className={`pi pi-bars ${style.btn_icon}`}></i>
-        </a>
+        <div className={style.botones}>
+          <button className={style.btn_grande}>Empezar</button>
+          <button className={style.btn_lng}>Ing</button>
+          <button className={style.btn_lng}>Esp</button>
+          <a href="#" className={style.btn_mobile}>
+            {" "}
+            <i className={`pi pi-bars ${style.btn_icon}`}></i>
+          </a>
+        </div>
       </div>
     </header>
   );

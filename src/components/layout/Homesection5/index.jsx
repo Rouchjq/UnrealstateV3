4f79615501ab98aec6624225774775af574f0fde
Jorge Quintero <rouchjq8@gmail.com>
2022-02-28 17:22:23 -0400

    crossorigin y sensibilidad
    Se agrego la etiqueta crossorigin y la sensibilidad del video en mobile

--- src/components/layout/Homesection5/index.jsx
+++ src/components/layout/Homesection5/index.jsx
@@ -49,18 +49,22 @@ export const Homesection5 = () => {
               id="my-player"
               class={`video-js ${style.reproductor}`}
               controls
+              crossorigin="anonymous"
               preload="auto"
               poster="//vjs.zencdn.net/v/oceans.png"
             >
-              <source src={vid} type="video/mp4"></source>
               {/* <source
+                src="vjs.zencdn.net/v/oceans.mp4"
+                type="video/mp4"
+              ></source> */}
+              <source
                 src="//vjs.zencdn.net/v/oceans.webm"
                 type="video/webm"
               ></source>
               <source
                 src="//vjs.zencdn.net/v/oceans.ogv"
                 type="video/ogg"
-              ></source> */}
+              ></source>
               <p class="vjs-no-js">
                 To view this video please enable JavaScript, and consider
                 upgrading to a web browser that
